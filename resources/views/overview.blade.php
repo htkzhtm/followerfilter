587e495f8a5175d9c5c3c5df48f9673a879d8f5e
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2019-02-09 20:01:31 +0900

    Modify Overview page.

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -1,7 +1,61 @@
 @extends('layouts.home')
+@section('title', 'person')
 
+<link href="{{ asset('css/introduction/overview.css') }}" rel="stylesheet">
 
 @section('content')
-Name: hitomi
-
+<div class="panel-body">
+    <div class="row">
+    <div class=" col-md-9 col-lg-9 "> 
+    <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="pic/pic20190209.png" class="img-circle img-responsive"> </div>
+        <table class="table table-user-information">
+        <tbody>
+            <tr>
+            <td>Name:</td>
+            <td>Hitomi Hatsukaze</td>
+            </tr>
+            <tr>
+            <td>Gender:</td>
+            <td>Male</td>
+            </tr>
+            <tr>
+            <td>Age:</td>
+            <td>22~27</td>
+            </tr>
+            <tr>
+            <td>Prefecture:</td>
+            <td>Aichi Toyohashi -> Shimane Mastue -> Tokyo Chiyoda (Live in Saitama Omiya)</td>
+            </tr>
+            <tr>
+            <tr>
+            <td>Department:</td>
+            <td>Engineer for Web</td>
+            </tr>
+            <tr>
+            <td>Language:</td>
+            <td>C++, Java, Perl, C#, PHP, JS</td>
+            </tr>
+            <td>Hobby:</td>
+            <td>Waking (Pokemon GO)</td>
+            </tr>
+            <tr>
+            <td>Favorite:</td>
+            <td>Cocktail (Based Gin Vodka, or Rum)</td>
+            </tr>
+            <tr>
+            <td>Dislike:</td>
+            <td>Raw fish</td>
+            </tr>
+            <td>Others:</td>
+            <td>
+                <a href="https://github.com/htkzhtm/pages" target="_blank"><i class="fab fa-github"></a></i>
+                <a href="https://twitter.com/intent/user?user_id=1044220714536992768" target="_blank"><i class="fab fa-twitter"></a></i>
+            </td>
+            </tr>
+            
+        </tbody>
+        </table>
+    </div>
+    </div>
+</div>
 @endsection
