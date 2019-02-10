677898aa31eeaf797dbe71d46e2537793e79a2ce
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2019-02-10 20:35:53 +0900

    DI Service.

--- app/Http/Controllers/Auth/TwitterController.php
+++ app/Http/Controllers/Auth/TwitterController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Services\TwitterService;
 use Socialite;
 
 class TwitterController extends Controller
 {
+
+    protected $twitter;
+
+    public function __construct(TwitterService $twitter) {
+        $this->twitter = $twitter;
+    }
+
     public function login(){
         return Socialite::driver('twitter')->redirect();
     }
@@ -15,11 +23,9 @@ class TwitterController extends Controller
     public function auth(){
 
         $user = Socialite::driver('twitter')->user();
-        echo('<pre>');
-        var_dump($user);
-        echo('<pre>');
+        $this->twitter->oauth($user);
         // Auth::login($user);
-        return redirect('twitter/home');
+        // return redirect('twitter/home');
     }
 
     public function logout()
