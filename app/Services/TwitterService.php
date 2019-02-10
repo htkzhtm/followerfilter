51ba59abfac597e2d9c92cca54830718708596a9
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2019-02-10 23:01:24 +0900

    Request User detail via token

--- app/Services/TwitterService.php
+++ app/Services/TwitterService.php
@@ -7,9 +7,18 @@ use Laravel\Socialite\One\User;
 
 class TwitterService {
 
-    public function oauth (User $user) {
+    public function oauth (User $user)
+    {
+        $twitter = new TwitterOAuth(
+            env("TWITTER_CLIENT_KEY"),
+            env("TWITTER_CLIENT_SECRET"),
+            $user->token,
+            $user->tokenSecret
+        );
+
         echo('<pre>');
-        var_dump($user);
+        var_dump(get_object_vars($twitter->get('account/verify_credentials')));
         echo('<pre>');
+
     }
 }
