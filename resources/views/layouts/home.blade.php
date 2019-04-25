e7c6b2094883e58d53feba96bca103799f3cfb1c
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2019-04-25 20:35:18 +0900

    remove sidebar

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -29,54 +29,6 @@ https://github.com/azouaoui-med/pro-sidebar-template
 </head>
 
 <body>
-    <div class="page-wrapper chiller-theme toggled">
-  <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
-    <i class="fas fa-bars"></i>
-  </a>
-  <nav id="sidebar" class="sidebar-wrapper">
-    <div class="sidebar-content">
-      <div class="sidebar-brand">
-          <a href="/">pro sidebar</a>
-      </div>
-      <!-- sidebar-header  -->
-
-      <!-- sidebar-search  -->
-      <div class="sidebar-menu">
-        <ul>
-          <li class="header-menu">
-            <span>Content</span>
-          </li>
-          <li class="sidebar-dropdown">
-            <a href="#">
-              <i class="fa fa-star"></i>
-              <span>Introduction</span>
-            </a>
-            <div class="sidebar-submenu">
-              <ul>
-                <li>
-                  <a href="/overview">Overview</a>
-                </li>
-                <li>
-                  <a href="detail">Detail this site</a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li class="header-menu">
-            <span>Extra</span>
-          </li>
-          <li>
-            <a href="twitter">
-            <i class="fab fa-twitter"></i>
-              <span>Twitter</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <!-- sidebar-menu  -->
-    </div>
-    <!-- sidebar-content  -->
-  </nav>
   <!-- sidebar-wrapper  -->
   <main class="page-content">
     <div class="container-fluid">
@@ -86,7 +38,7 @@ https://github.com/azouaoui-med/pro-sidebar-template
     </div>
   </main>
   <!-- page-content" -->
-</div>
+
 <!-- page-wrapper -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
